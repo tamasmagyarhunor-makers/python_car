5adda8ee01e53fae5bc129b6f42c087e73bf5298
Magyar-Hunor Tamas <hunor@makers.tech>
2022-06-10 00:34:55 +0100

    Fix response json order

--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 # creating an instance of the flask app
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False # keep this in mind when you do this: https://stackoverflow.com/a/43263483
 
 # Configure our database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
