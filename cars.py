fd21d98463df1c269e11650e4b17c43ed9b9fc7d
Magyar-Hunor Tamas <hunor@makers.tech>
2022-06-09 23:26:16 +0100

    Finish CRUD for Car model

--- cars.py
+++ cars.py
@@ -24,16 +24,38 @@ class Car(db.Model):
             'year': self.year
         } # this method will convert our output to json
 
-    def add_car(_make, _model, _fuel_type, _gerbox, _year):
-        '''CREATE method'''
+    def add_car(_make, _model, _fuel_type, _gearbox, _year):
+        '''CREATE new Car'''
         # create an instance of Car constructor
         new_car = Car(
             make = _make,
             model = _model,
             fuel_type = _fuel_type, 
-            gearbox = _gerbox,
+            gearbox = _gearbox,
             year = _year
         )
 
         db.session.add(new_car) # add movie to db session
         db.session.commit() # commit changes to session
+
+    def get_all_cars():
+        '''GET all Cars'''
+        return [Car.json(car) for car in Car.query.all()]
+
+    def get_car(_id):
+        '''GET a Car by id'''
+        return [Car.json(Car.query.filter_by(id = _id).first())]
+
+    def update_car(_id, _make, _model, _fuel_type, _gearbox, _year):
+        '''UPDATE a car by id'''
+        car_to_update = Car.query.filter_by(id = _id).first()
+        car_to_update.make = _make
+        car_to_update.model = _model
+        car_to_update.fuel_type = _fuel_type
+        car_to_update.gearbox = _gearbox
+        db.session.commit()
+
+    def delete_car(_id):
+        '''DELETE a car by id'''
+        Car.query.filter_by(id = _id).delete()
+        db.session.commit()
